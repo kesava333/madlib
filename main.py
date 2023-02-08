7e003bd6101e532544df488a79f155820e15d56c
Kesava Ginjupalli <kesichow333@gmail.com>
2023-02-07 21:22:14 -0500

    done with first project

---
+++ main.py
@@ -0,0 +1,9 @@
+v1= input("what is he:")
+v2= input("what is very special about him:")
+v3 = input("whats his name")
+madlib = f"{v3} is an {v1}, who always inspires everyone all over the globe. something very special about {v3} is that he has unique \
+     {v2}"
+     
+print(madlib)
+
+
